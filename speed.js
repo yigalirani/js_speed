58e77631dd5ed7ba4585406c2729a2ebeb40443e
Yigal Irani <yigal@symbolclick.com>
2022-08-17 15:02:52 -0400

    fixed bugs, json printouts,added class implmentation

--- speed.js
+++ speed.js
@@ -1,3 +1,4 @@
+'use strict';
 var run_counter=0;
 function build(depth){
   if (depth==0)
@@ -12,42 +13,57 @@ function count(node){
 function build2(depth){
   if (depth==0)
     return null
-  return {v:run_counter++,left:build(depth-1),right:build(depth-1)}
+  return {v:run_counter++,left:build2(depth-1),right:build2(depth-1)}
 }
 function count2(node){
   if (node==null)
     return 0
-  return 1+count(node.left)+count(node.right)
+  return 1+count2(node.left)+count2(node.right)
 }
-function measure(title,f){
-  var start=Date.now()
-  var ops=f()
-  var end=Date.now()
-  var diff=(end-start)/1000
-  console.log(title,'elapsed',diff,'ops',ops,'meg_ops_persecs',ops/diff/1000000)
-  print_mem()
-}
-function print_mem(){
-  try{
-    var used=process.memoryUsage().heapTotal
-    console.log('used',used,'pernode',used/run_counter)
-  }catch(e){
-    //console.log(e)
+class Node{
+  constructor(v,left,right){
+    this.v=v
+    this.left=left
+    this.right=right
   }
 }
-depth=24 //number is chosen to max out memory and force gc
-measure('array',()=>{
-  var root=build(depth)
-  var ops=count(root)
-  if (ops!=run_counter){
-    console.log('missmatch')
-  }
-  return ops
-})
+function build3(depth){
+  if (depth==0)
+    return null
+  
+  return new Node(run_counter++,build3(depth-1),build3(depth-1))
+}
+function count3(node){
+  if (node==null)
+    return 0
+  return 1+count3(node.left)+count3(node.right)
+}
 
-run_counter=0
-measure('dict',()=>{
-  var root=build2(depth)
-  ops=count2(root)
-  return ops
-})
+function measure(build,count){
+  var start=Date.now()
+  run_counter=0
+  var used_before=process.memoryUsage().heapTotal
+  var root=build(depth)
+  var used_build=process.memoryUsage().heapTotal
+  var ops=count(root)
+  root=null
+  var used_clear=process.memoryUsage().heapTotal
+  var used_diff=used_clear-used_before
+  var end=Date.now()
+  if (ops!=run_counter){
+    var error='missmatch'
+  }
+  var time_diff=(end-start)/1000
+  var meg_ops_persecs=ops/time_diff/1000000
+ 
+  var used_pernode=used_diff/run_counter
+  return {start,ops,end,time_diff,meg_ops_persecs,run_counter,error,used_before,used_build,used_clear,used_diff,used_pernode}
+}
+
+var depth=24 //number is chosen to max out memory and force gc
+var stats={
+  array:measure(build,count),
+  dict:measure(build2,count2),
+  class:measure(build3,count3)
+}
+console.log(JSON.stringify(stats,null,4))
